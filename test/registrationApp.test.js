f9f178c6b5bf3c3e6202238c1225c50aa460a019
TendaniMamadi <mamadi.tendani@gmail.com>
2023-08-31 15:09:41 +0200

    all file seem cool

--- test/registrationApp.test.js
+++ test/registrationApp.test.js
@@ -5,7 +5,7 @@ describe('registration_numbers', function () {
 
     it('Should display registration for Cape Town', function () {
 
-        const RegistrationApp = registrationApp();
+        let RegistrationApp = registrationApp();
         RegistrationApp.setDisplayRegistration("CA 98765");
 
         assert.equal("CA 98765", RegistrationApp.getDisplayRegistration());
@@ -14,7 +14,7 @@ describe('registration_numbers', function () {
 
     it('Should display registration for Johannesburg', function () {
 
-        const RegistrationApp = registrationApp();
+        let RegistrationApp = registrationApp();
         RegistrationApp.setDisplayRegistration("CZH 182 GP");
 
         assert.equal("CZH 182 GP", RegistrationApp.getDisplayRegistration());
@@ -23,7 +23,7 @@ describe('registration_numbers', function () {
 
     it('Should display registration for Durban', function () {
 
-        const RegistrationApp = registrationApp();
+        let RegistrationApp = registrationApp();
         RegistrationApp.setDisplayRegistration("ND 98765");
 
         assert.equal("ND 98765", RegistrationApp.getDisplayRegistration());
@@ -33,7 +33,7 @@ describe('registration_numbers', function () {
 
     it('Should display registration for Polokwane', function () {
 
-        const RegistrationApp = registrationApp();
+        let RegistrationApp = registrationApp();
         RegistrationApp.setDisplayRegistration("BCC 230 L");
 
         assert.equal("BCC 230 L", RegistrationApp.getDisplayRegistration());
