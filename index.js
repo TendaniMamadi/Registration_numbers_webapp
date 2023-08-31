31ecfa5c3df8e9899282bd18c6cb55ff17153d20
TendaniMamadi <mamadi.tendani@gmail.com>
2023-08-31 13:32:52 +0200

    setting up repo

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {engine} from 'express-handlebars';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+
+app.engine('handlebars', engine({
+    layoutsDir: './views/layouts'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+const PORT =process.env.PORT || 2023;
+app.listen(PORT,(req,res) =>{
+    console.log('We taking off on port:',PORT)
+});
