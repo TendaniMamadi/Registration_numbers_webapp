f8a53781bf20bb7a7ffadeaf53caab7fbc67bbfb
TendaniMamadi <mamadi.tendani@gmail.com>
2023-09-22 15:58:48 +0200

    trying to add function that catch duplicates, write a test,separate routes and style the app

--- database/db_queries.js
+++ database/db_queries.js
@@ -17,26 +17,38 @@ export default function db_queries(db) {
     }
 
     async function insertIntoRegistrationPlateNumber(registration_number) {
-        // Define the updated regex pattern here
+
         //valid formats: CA123123
-        //
-        let regEx = /^(CA|CJ|ND|CY)\s?\d{1,3}\s?\d{1,3}$/i
-        ;
-        
-        // Check if the registration_number matches the regex pattern
-        if (regEx.test(registration_number)) {
-            try {
-                let city_id = await getCityID(registration_number);
-                if (city_id) {
-                    await db.none(`INSERT INTO registrations (registration_number, city_id) VALUES ($1, $2)`, [registration_number, city_id.city_id]);
-                }
-            } catch (error) {
-                throw new Error('Error inserting registration: ' + error.message);
-            }
+        // Convert the entered registration number to lowercase for case insensitivity
+        let normalizedInput = registration_number.toLowerCase();
+        // Check if the normalized input exists in the array of registration numbers
+        if (registration_number.includes(normalizedInput)) {
+            // If it exists, send an error message
+            return "reg number already exists";
         } else {
-            // Handle the case where the registration_number doesn't match the regex pattern
-            let message = "Invalid registration number format"
-            return message
+
+                //if above condition is false, insert registration
+
+
+            //
+            let regEx = /^(CA|CJ|ND|CY)\s?\d{1,3}\s?\d{1,3}$/i
+                ;
+
+            // Check if the registration_number matches the regex pattern
+            if (regEx.test(registration_number)) {
+                try {
+                    let city_id = await getCityID(registration_number);
+                    if (city_id) {
+                        await db.none(`INSERT INTO registrations (registration_number, city_id) VALUES ($1, $2)`, [registration_number, city_id.city_id]);
+                    }
+                } catch (error) {
+                    throw new Error('Error inserting registration: ' + error.message);
+                }
+            } else {
+                // Handle the case where the registration_number doesn't match the regex pattern
+                let message = "Invalid registration number format"
+                return message
+            }
         }
     }
 
@@ -47,7 +59,7 @@ export default function db_queries(db) {
 
         try {
             registrations = await db.manyOrNone('SELECT registration_number FROM registrations');
-            
+
 
         } catch (error) {
             throw error
@@ -63,15 +75,35 @@ export default function db_queries(db) {
         if (city) {
             const query = `SELECT registration_number FROM registrations WHERE city_id = $1`
             let selectedCity = await getCityID(city);
-           
+
             let filt = await db.any(query, [selectedCity.city_id]);
-         
+
             return (filt);
         } else {
             const allQuery = 'SELECT registration_number FROM registrations';
             return await db.any(allQuery);
         }
     }
+
+    //functions that picks up duplication of data
+
+    // function avoidDuplications(registration_number) {
+    //     // // Convert the entered registration number to lowercase for case insensitivity
+    //     // let normalizedInput = registration_number.toLowerCase();
+
+
+
+    //     if (registration_number) {
+    //         // Check if the normalized input exists in the array of registration numbers
+    //         if (registration_number.includes(normalizedInput)) {
+    //             // If it exists, send an error message
+    //             return "reg number already exists";
+    //         }
+    //     }
+
+    //     // If no duplicate is detected, add registration.
+
+    // }
 
 
 
@@ -91,7 +123,8 @@ export default function db_queries(db) {
         getAllRegistrations,
         filterRegistrationsByCity,
         getCityID,
-        deleteRegistrations
+        deleteRegistrations,
+        // avoidDuplications
     }
 
 }
