93395c72fd842245292248d02316a2dc00fc72e4
TendaniMamadi <mamadi.tendani@gmail.com>
2023-10-05 21:27:00 +0200

    i'm not sure how to get it to flash invalid format

--- routes/routes.js
+++ routes/routes.js
@@ -12,22 +12,22 @@ export default function routes(frontendInstance, logic) {
         if (reg.length >= 5) {
             let regEx = frontendInstance.errorMessage(registrationNumber);
             let catchingDuplicates = await logic.alreadyExistInDatabase(registrationNumber);
-    
+
             if (!regEx) {
                 if (catchingDuplicates) {
                     req.flash('alreadyExists', 'Registration entered already exists');
                 } else {
                     if (await logic.getCityID(registrationNumber) == null) {
-    
-                        req.flash('notValid', 'City ID for registration entered is not supported');
-    
+
+                        req.flash('notValid', 'Registration entered is not supported');
+
                     } else {
-    
+
                         await logic.insertIntoRegistrationPlateNumber(registrationNumber);
                         req.flash('valid', 'Registration number added successfully');
                     }
                 }
-    
+
             }
         } else {
             req.flash('notValid', 'Reg Too short');
@@ -43,7 +43,7 @@ export default function routes(frontendInstance, logic) {
         const selectedCity = req.body.city;
         const filteredPlates = await logic.filterRegistrationsByCity(selectedCity);
 
-        if(filteredPlates == ''){
+        if (filteredPlates == '') {
             req.flash('notValid', 'No registration for selected city')
         }
         res.render('index', { filteredPlates });
@@ -62,3 +62,42 @@ export default function routes(frontendInstance, logic) {
         clearingRoute
     }
 }
+
+
+
+
+
+
+// const insertRoute = async (req, res) => {
+//     // Access input data using req.body
+//     const registrationNumber = req.body.number;
+//     let reg = registrationNumber.substring(2)
+
+//     if (reg.length >= 5) {
+//         req.flash('notValid', 'Reg Too short');
+//     }
+
+
+//     let catchingDuplicates = await logic.alreadyExistInDatabase(registrationNumber);
+//     if (catchingDuplicates) {
+//         req.flash('alreadyExists', 'Registration entered already exists');
+//     }
+
+//     let regEx = frontendInstance.errorMessage(registrationNumber);
+//     if (!regEx) {
+//         if (await logic.getCityID(registrationNumber) == null) {
+
+//             req.flash('notValid', 'Registration entered is not supported');
+
+//         } else {
+
+//             await logic.insertIntoRegistrationPlateNumber(registrationNumber);
+//             req.flash('valid', 'Registration number added successfully');
+//         }
+//     }
+
+
+
+
+//     res.redirect('/');
+// }
